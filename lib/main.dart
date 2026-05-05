ea03dc558d2ca10784c8376a986757199615f352
Stephen Lloyd <stephen.lloyd03@gmail.com>
2026-05-05 11:18:16 +0200

    codelabs step 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+const clientId = 'YOUR_CLIENT_ID';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp(clientId: clientId));
 }
 
 class MyApp extends StatelessWidget {
